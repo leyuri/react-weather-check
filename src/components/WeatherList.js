8208c678eae553f6661da876ee7e5d878c89af61
leyuri <leyuri97@gmail.com>
2021-08-08 20:17:09 +0900

    Move data from WeatherList to Reducer & Add state value call using useSelector

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,47 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Table } from 'react-bootstrap';
-import WeatherItem from './WeatherItem'
+import WeatherItem from './WeatherItem';
 
 export default function WeatherList() {
 
-    const data = [
-        {
-            id: 1,
-            city: 'Seoul',
-            temp: 12.89,
-            pressure: 1018,
-            humidity: 68,
-        },
-        {
-            id: 2,
-            city: 'Tokyo',
-            temp: 12.89,
-            pressure: 1018,
-            humidity: 68,
-        },
-        {
-            id: 3,
-            city: 'Istanbul',
-            temp: 12.89,
-            pressure: 1018,
-            humidity: 23,
-        },
-        {
-            id: 4,
-            city: 'Paris',
-            temp: 12.89,
-            pressure: 1018,
-            humidity: 44,
-        },
-        {
-            id: 5,
-            city: 'Berlin',
-            temp: 12.89,
-            pressure: 2019,
-            humidity: 70,
-        },
-    ]
-
+    const cities = useSelector((state) => state.cities) 
     return (
         <div>
             <Table className="mt-2" striped bordered hover>
@@ -54,7 +18,7 @@ export default function WeatherList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map(item => (<WeatherItem data={item} />))}
+                    {cities.map(item => (<WeatherItem data={item} />))}
                 </tbody>
             </Table>
         </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,44 @@
-import produce from "immer"
+import produce from "immer";
 
-const initialState = {};
+const initialState = {
+    cities : [
+        {
+            id: 1,
+            city: 'Seoul',
+            temp: 12.89,
+            pressure: 1018,
+            humidity: 68,
+        },
+        {
+            id: 2,
+            city: 'Tokyo',
+            temp: 12.89,
+            pressure: 1018,
+            humidity: 68,
+        },
+        {
+            id: 3,
+            city: 'Istanbul',
+            temp: 12.89,
+            pressure: 1018,
+            humidity: 23,
+        },
+        {
+            id: 4,
+            city: 'Paris',
+            temp: 12.89,
+            pressure: 1018,
+            humidity: 44,
+        },
+        {
+            id: 5,
+            city: 'Berlin',
+            temp: 12.89,
+            pressure: 2019,
+            humidity: 70,
+        },
+    ]
+};
 const reducer = produce((state, action) => {
     switch(action.type) {
         default:
